abb37e4142e1652e922a6c9f865515707b93ae4f
r720 <963838470@qq.com>
2019-06-30 12:06:45 +0800

    调用注册过的服务

---
+++ account-service/src/main/java/com/chenbo/accountservice/service/BalanceServiceFallback.java
@@ -0,0 +1,12 @@
+package com.chenbo.accountservice.service;
+
+import com.chenbo.accountservice.model.Balance;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BalanceServiceFallback implements BalanceService {
+    @Override
+    public Balance getBalance(Integer id) {
+        return new Balance(0, 0, 0, "降级");
+    }
+}
