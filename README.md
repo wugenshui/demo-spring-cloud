65b592324bb7cfee3a464a24f609d7013b265a9d
r720 <963838470@qq.com>
2019-12-24 21:16:58 +0800

    项目重置

--- README.md
+++
@@ -1,2 +0,0 @@
-# demo-spring-cloud
-spring cloud alibaba demo
---
+++ src/main/java/com/chenbo/demospringcloud/DemoSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.chenbo.demospringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringCloudApplication.class, args);
+    }
+
+}
