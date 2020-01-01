ff1e270205397e10d49c5bb31ea8b4fae65255e7
r720 <963838470@qq.com>
2020-01-01 23:27:14 +0800

    dubbo消费者

---
+++ hello-apache-dubbo-consumer/src/main/java/com/chenbo/apache/dubbo/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.chenbo.apache.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenbo
+ * @date : 2020-01-01
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ hello-apache-dubbo-consumer/src/main/java/com/chenbo/apache/dubbo/consumer/controller/EchoController.java
@@ -0,0 +1,21 @@
+package com.chenbo.apache.dubbo.consumer.controller;
+
+import com.chenbo.apache.dubbo.provider.api.EchoService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : chenbo
+ * @date : 2020-01-01
+ */
+@RestController
+public class EchoController {
+    @Reference(version = "1.0.0")
+    private EchoService echoService;
+    @GetMapping(value = "/echo/{string}")
+    public String echo(@PathVariable String string) {
+        return echoService.echo(string);
+    }
+}
