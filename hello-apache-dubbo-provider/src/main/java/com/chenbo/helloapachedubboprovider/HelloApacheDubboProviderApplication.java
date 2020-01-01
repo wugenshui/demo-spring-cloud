9355821e6f7e09dd9f40cb02428ae2c1289e9ef6
r720 <963838470@qq.com>
2020-01-01 21:45:24 +0800

    denpendencies+provider

---
+++ hello-apache-dubbo-provider/src/main/java/com/chenbo/helloapachedubboprovider/HelloApacheDubboProviderApplication.java
@@ -0,0 +1,13 @@
+package com.chenbo.helloapachedubboprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApacheDubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApacheDubboProviderApplication.class, args);
+    }
+
+}
