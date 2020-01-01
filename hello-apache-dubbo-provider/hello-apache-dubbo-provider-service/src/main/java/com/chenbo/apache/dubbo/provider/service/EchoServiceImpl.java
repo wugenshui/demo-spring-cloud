4f59640cb00ce5e4d9283418830b49551e4f3b14
r720 <963838470@qq.com>
2020-01-01 23:39:18 +0800

    dubbo配置负载均衡测试

--- hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/chenbo/apache/dubbo/provider/service/EchoServiceImpl.java
+++ hello-apache-dubbo-provider/hello-apache-dubbo-provider-service/src/main/java/com/chenbo/apache/dubbo/provider/service/EchoServiceImpl.java
@@ -2,6 +2,7 @@ package com.chenbo.apache.dubbo.provider.service;
 
 import com.chenbo.apache.dubbo.provider.api.EchoService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author : chenbo
@@ -9,8 +10,11 @@ import org.apache.dubbo.config.annotation.Service;
  */
 @Service(version = "1.0.0")
 public class EchoServiceImpl implements EchoService {
+    @Value("${dubbo.protocol.port}")
+    private String port;
+
     @Override
     public String echo(String string) {
-        return "Echo Hello Dubbo " + string;
+        return "Echo Hello Dubbo " + string + " i am from port: " + port;
     }
 }
