5a264d65d5e08e4680ab7806c3927b28c1a546b3
wugenshui <963838470@qq.com>
2020-01-02 16:26:00 +0800

    OAuth2 init

---
+++ hello-spring-security-oauth2-server/src/main/java/com/chenbo/hellospringsecurityoauth2server/HelloSpringSecurityOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.chenbo.hellospringsecurityoauth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringSecurityOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringSecurityOauth2ServerApplication.class, args);
+    }
+
+}
